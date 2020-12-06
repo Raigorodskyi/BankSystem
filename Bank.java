f6b6f371a4cdc91faaa0158aea4291dc15f629d8
Raigorodskyi <raigorodskyi@gmail.com>
2020-12-06 13:57:22 -0500

    Modifications

--- Bank.java
+++ Bank.java
@@ -4,11 +4,25 @@
  * and open the template in the editor.
  */
 package banksystem;
-
+import java.util.ArrayList;
 /**
  *
  * @author 2003i
  */
 public class Bank {
+    private static ArrayList<AutomatedTellerMachine> atms = new ArrayList<>();
+    private static ArrayList<User> users = new ArrayList<>();
     
+    /**
+     * To add a new user to the user list
+     * @param userName the name of the user
+     * @param pin the pin of the user
+     */
+    public static void addUser(String userName, String pin) {
+        users.add(new User(userName, pin));
+    }
+
+    public static ArrayList<User> getUsers() {
+        return users;
+    }
 }
